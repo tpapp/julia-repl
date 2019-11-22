f1006baa09527fcccb649ffbdd7286282b5a136a
Tamas K. Papp <tkpapp@gmail.com>
2019-11-22 17:12:47 +0100

    add unit testing framework

---
+++ julia-repl-tests.el
@@ -0,0 +1,11 @@
+;;; julia-repl-tests.el ---                          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Tamas Papp
+;; Author: Tamas Papp <tkpapp@gmail.com>
+
+(require 'cl-lib)
+(require 'julia-repl)
+(require 'ert)
+
+(ert-deftest dummy-test ()
+  (should t))
